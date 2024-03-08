753f259d207d47072e51786d8799e4dc7e685d35
lemuellin <lemuellin@gmail.com>
2024-03-08 15:46:34 -0800

    add counter

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import Header from './components/Header';
 import Content from './components/Content';
 import Contact from './components/Contact'
 
+const countapi = require('countapi-js');
+
 function App() {
+  countapi.visits().then((result) => {
+    console.log(result.value);
+  });
+  
   return (
     <div id="page-container">
       <Header/>
       <div id = "content-wrap">
         <Content/>
       </div>
+      <div id="visits">...</div>
       <footer id="footer">
         <Contact/>
       </footer>
